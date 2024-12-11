79e7d91d71ed85746dcb85306db96b9a04aa616a
Alanimustafa <mustafa.kamel.ani@gmail.com>
2024-12-10 18:02:56 -0600

    ATM Quick WithDrawal - Complete

--- app.js
+++ app.js
@@ -1,27 +1,122 @@
+
+// The Date Function
 function getCurrentDate() {
   const now = new Date();
-  return now.toLocaleDateString(); // e.g., "MM/DD/YYYY" format
+  return now.toLocaleDateString();
 }
 
-// Function to format the time
+// The Time Function
 function getCurrentTime() {
   const now = new Date();
-  return now.toLocaleTimeString(); // e.g., "HH:MM:SS AM/PM" format
+  return now.toLocaleTimeString();
 }
 
-// Inserting date and time into respective divs
+// Inserting date and time into the header
 document.querySelector('.todayDate').textContent = `${getCurrentDate()}`;
 document.querySelector('.currentTime').textContent = `${getCurrentTime()}`;
 
-// Call the function initially to set the date and time
 
 
-// Update the date and time every second
-setInterval(getCurrentTime(), 1000);
+//-------------------------------------------------------------------------------
+
+// Links to the ATM html elements
+let mainScreen = document.querySelector('.mainScreen');
+let theScreen = document.querySelector('.mainScreenText');
+
+// Enter pin Function
+function enterPinNumber () {
+
+}
 
 
+// Quick Withdrawal Functoin
+function withDrawal () {
+  
+  // The Function Variables.
+    const functionLabel = document.querySelector('.mainScreenText');
+    const enterAmount = document.createElement('input');
+    const submitButton = document.createElement('button');
+
+  // Assigning the Classes to the Divs
+    functionLabel.textContent = "Quick Withdrawal";
+    enterAmount.setAttribute("class", "mainScreenText");
+    submitButton.setAttribute("class", "btnCSS");
+
+  // The Submit Button
+    submitButton.textContent = "Confirme"
+    submitButton.style.margin = "1em"
+    submitButton.addEventListener('click', 
+      
+      
+      // Function when a submit button click.
+      function imClicked () {
+        submitButton.style.display = "none";
+        
+        const theReport = document.createElement ('div');
+        const goHomeButton = document.createElement ('button');
+        goHomeButton.setAttribute("class", "btnCSS");
+        goHomeButton.textContent = "Home";
+        goHomeButton.style.marginLeft = "100px;"
+        goHomeButton.style.marginTop = "90px"
+        goHomeButton.addEventListener('click', () => {
+          theScreen.innerHTML = "Thank you!";
+        })
 
 
+        theReport.setAttribute("class", "mainScreenText");
+        theReport.style.fontSize = "30px";
+        theReport.textContent = enterAmount.value + " - has been withdrawaled.";
+        theReport.style.marginTop = "1em";
+        theScreen.append(theReport);
+        theScreen.append(goHomeButton);
+        
+        
+      }
+    )
+
+  // The Input field
+    enterAmount.type="text";
+    enterAmount.placeholder="$0.00";
+    enterAmount.defaultValue = "$ ";
+    enterAmount.style.background ="none";
+    enterAmount.style.marginTop ="1em";
+    enterAmount.style.border ="solid lightbule 2px";
+
+    
+    theScreen.append(enterAmount);
+    mainScreen.append(submitButton);
+
+}
+
+
+// Cash Transactions Function
+function cashTransaction () {
+
+}
+
+
+// Balance Statement Function
+function balanceStatement () {
+
+}
+
+
+// Payments Function
+function payments () {
+
+}
+
+
+// Online Banking Function
+function onlineBanking () {
+
+}
+
+
+// other Services Function
+function otherServices () {
+
+}
 
 
 
